8696af3e1aba9d1f3ff8f88c8f32abee09c68979
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-09-11 16:01:17 +0100

    Add navbar and home/profile components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Profile from './Profile';
+import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Should work
-      </p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/profile" component={Profile} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img id="logo" src="https://picsum.photos/200/300" alt="logo" width="50" height="50" />
+      <ul className="navbar-links">
+        <Link to="/">
+          <li className="navbar-links-item">
+            Home
+          </li>
+        </Link>
+        <Link to="/profile">
+          <li className="navbar-links-item">
+            Profile
+          </li>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
