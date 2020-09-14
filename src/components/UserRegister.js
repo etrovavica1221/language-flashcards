6ee4c67f21c472ba7f64740e2611e38a1f0ae7ee
niallgreene7 <niall251@googlemail.com>
2020-09-14 11:59:58 +0100

    login and register forms created

--- src/components/UserRegister.js
+++ src/components/UserRegister.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import '../styles/LoginRegister.css';
 
 const userRegister = () => (
   <div className="UserRegister">
-    User Register
+        <h1>Register</h1>
+        <form action="submit">
+            <input type="text" placeholder="Email Address" required/>
+            <input type="password" placeholder="Password" required/>
+            <input type="password" placeholder="Confirm Password" required/>
+            <select defaultValue="Translate From" required>
+                <option value="EN">Translate From</option>
+                <option value="EN">English</option>
+                <option value="RU">Russian</option>
+                <option value="CH">Chinese</option>
+            </select>
+            <select defaultValue="Translate To" required>
+                <option value="EN">Translate To</option>
+                <option value="EN">English</option>
+                <option value="RU">Russian</option>
+                <option value="CH">Chinese</option>
+            </select>
+            <button type="submit">Register</button>
+        
+        </form>
   </div>
 );
 
--- src/components/Userlogin.js
+++ src/components/Userlogin.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/LoginRegister.css';
 
 const userLogin = () => (
   <div className="UserLogin">
-    User Login
+    <h1>Log In</h1>
+    <form action="submit">
+        <input type="text" placeholder="Email Address" required/>
+        <input type="password" placeholder="Password" required/>
+        <button type="submit">Login</button>
+        <Link className="hyperlink" to="/register">Not a member? Register here.</Link>
+    </form>
   </div>
 );
 
