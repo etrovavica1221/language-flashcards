703194132e47957ac41143f30d378c418b72f69e
WilPowell1 <powellwil88@gmail.com>
2020-09-24 20:58:31 +0100

    translator component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,13 @@
 /* eslint-disable spaced-comment */
 /* eslint-disable quotes */
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
+import { setRawCookie } from 'react-cookies';
 import { Link } from 'react-router-dom';
+import cookies from "react-cookies";
 import '../styles/Home.css';
+
+import { googleTranslate } from '../utils/googleTranslate';
 
 const translateState = {
   value: '',
@@ -49,6 +53,85 @@ const Home = ({ userState, setUserState }) => {
     saveButton = "";
   }
 
+  class App extends Component {
+    state = {
+      languageCodes: [],
+      language: cookie.load("language") ? cookie.load("language") : "en",
+      question: cookie.load("question")
+        ? cookie.load("question")
+        : "What language do you prefer to read with?"
+    };
+  
+    componentDidMount() {
+      // load all of the language options from Google Translate to your app state
+  
+      googleTranslate.getSupportedLanguages("en", function(err, languageCodes) {
+        getLanguageCodes(languageCodes); // use a callback function to setState
+      });
+  
+      const getLanguageCodes = languageCodes => {
+        this.setState({ languageCodes });
+      };
+    }
+  
+    render() {
+      const { languageCodes, language, question } = this.state;
+  
+      return (
+        <div style={this.divStyle}>
+          <p>{question}</p>
+  
+          {/* iterate through language options to create a select box */}
+          <select
+            className="select-language"
+            value={language}
+            onChange={e => this.changeHandler(e.target.value)}
+          >
+            {languageCodes.map(lang => (
+              <option key={lang.language} value={lang.language}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      );
+    }
+  
+    changeHandler = language => {
+      let { question } = this.state;
+      let cookieLanguage = cookie.load("language");
+      let transQuestion = "";
+  
+      const translating = transQuestion => {
+        if (question !== transQuestion) {
+          this.setState({ question: transQuestion });
+          cookie.save("question", transQuestion, { path: "/" });
+        }
+      };
+  
+      // translate the question when selecting a different language
+      if (language !== cookieLanguage) {
+        googleTranslate.translate(question, language, function(err, translation) {
+          transQuestion = translation.translatedText;
+          translating(transQuestion);
+        });
+      }
+  
+      this.setState({ language });
+      cookie.save("language", language, { path: "/" });
+    };
+  
+    // just some inline css to center our demo
+    divStyle = {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      width: "100wh"
+    };
+  }
+
+
   return (
     <div id="HomePage">
       <div id="landing">
@@ -62,7 +145,7 @@ const Home = ({ userState, setUserState }) => {
         <h1 className="titles">Translate your text and make your own flashcards</h1>
           <form id="translation-form" action="submit">
             <textarea type="text" placeholder="Enter text to translate..." required name="textInput" onChange={handleChange} />
-            <textarea value={Value.value} type="text" placeholder="Translation..." required name="textOutput" />
+            <textarea value={Value.value} type="text" placeholder="Translation..." required name="textOutput"/>
           </form>
         {saveButton}
       </div>
