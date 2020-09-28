a0df5005423c749580320ebe78373c738f5ef466
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-09-28 12:19:31 +0100

    Correct buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,8 +30,8 @@ const Home = ({ userState, setUserState }) => {
   //home page definition
   if (isLoggedIn) {
     message = <h1 className="titles">Welcome back, {userState.forename}!</h1>;
-    button = <Link to="/learn"><button className="base-button" type="button">View My Flashcards!</button></Link>;
-    logout = <Link to="/"><button className="logOut" id="large-logOut-button" type="button" onClick={handleLogout}>Log Out</button></Link>;
+    button = <Link to="/learn"><button className="base-button" id="large-home-button" type="button">View My Flashcards!</button></Link>;
+    logout = <Link to="/"><button className="base-button" id="large-logOut-button" type="button" onClick={handleLogout}>Log Out</button></Link>;
   } else {
     message = <h1 className='titles'>Welcome to LingoGuru!<div className='titles'>Please log in</div></h1>;
     button = <Link to="/login"><button className="base-button" type="button">Log in</button></Link>;
@@ -45,8 +45,10 @@ const Home = ({ userState, setUserState }) => {
       <div id="landing">
       {message}
       <br></br>
-      {button}
-      {logout}
+      <div id="home-buttons-container">
+        {button}
+        {logout}
+      </div>
       </div>
       <div id="footer-container">
         <footer>
